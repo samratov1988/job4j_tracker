6d5f0ac8c2d35eaede0d99964bbd4995652ff538
samratov1988 <samratov1988@gmail.com>
2021-07-03 17:29:24 +0600

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,44 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public void printInfo() {
+        System.out.println("Активна: " + active);
+        System.out.println("Код ошибки: " + status);
+        System.out.println("Сообщение: " + message);
+        System.out.println();
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+        active = true;
+        status = 505;
+        message = "Устаревший браузер";
+    }
+
+    public static void main(String[] args) {
+
+        Error error404 = new Error(true, 404, "не найдено");
+        error404.printInfo();
+
+        Error error405 = new Error(true, 405, "метод запроса не используется");
+        error405.printInfo();
+
+        Error error408 = new Error(true, 408, "время ожидания истекло");
+        error408.printInfo();
+
+        Error error505 = new Error();
+        error505.printInfo();
+    }
+}

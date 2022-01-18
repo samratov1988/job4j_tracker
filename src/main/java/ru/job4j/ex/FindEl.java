b5e871c220f2e317e8532ec308623f2e2dbb1e73
samratov1988 <samratov1988@gmail.com>
2022-01-18 17:16:18 +0600

    0.4. Пользовательские исключения. [#219367 #249112]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,7 +1,5 @@
 package ru.job4j.ex;
 
-import java.util.concurrent.ExecutionException;
-
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
@@ -15,21 +13,6 @@ public class FindEl {
             throw new ElementNotFoundException("Item not found.");
         }
         return rsl;
-    }
-
-    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
-
-        return true;
-    }
-
-    public static void process(String[] value, String key, String[] abuses) {
-        try {
-            if (indexOf(value, key) != -1) {
-                sent(key, abuses);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {

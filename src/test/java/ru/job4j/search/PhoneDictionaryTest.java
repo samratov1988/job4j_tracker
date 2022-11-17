c29a60b43df0f5723982e8d13ac21fa30ab36241
samratov1988 <samratov1988@gmail.com>
2022-11-17 23:40:39 +0600

    7. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,28 +1,19 @@
 package ru.job4j.search;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PhoneDictionaryTest {
-
     @Test
     public void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
-        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
         ArrayList<Person> persons = phones.find("Petr");
-        assertThat(persons.get(0).getSurname(), is("Arsentev"));
-    }
-
-    @Test
-    public void whenFindByNull() {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> persons = phones.find("Samat");
-        ArrayList<Person> personsNull = new ArrayList<>();
-        assertThat(persons, is(personsNull));
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 }

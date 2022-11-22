ee70bd67cddf0198215cb36ca2a71ca085f58dd8
samratov1988 <samratov1988@gmail.com>
2022-11-22 20:20:18 +0600

    14. Агрегатные методы min(), max(), count(), average(), sum() [#504928]

---
+++ src/main/java/ru/job4j/stream/AverageExample.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class AverageExample {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        OptionalDouble average = people.stream()
+                .mapToInt(Person::getAge)
+                .average();
+        double avg = average.getAsDouble();
+        System.out.println(avg);
+    }
+}

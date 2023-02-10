ebb107a1198b4fa2d1e5565ddecb2e7f3b79ec93
samratov1988 <samratov1988@gmail.com>
2023-02-11 00:44:21 +0600

    Обновление Версии JDK

--- src/main/java/ru/job4j/map/Subject.java
+++ src/main/java/ru/job4j/map/Subject.java
@@ -2,53 +2,41 @@ package ru.job4j.map;
 
 import java.util.Objects;
 
-public class Subject {
-    private String name;
-    private int score;
+public final class Subject {
+    private final String name;
+    private final int score;
 
     public Subject(String name, int score) {
         this.name = name;
         this.score = score;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Subject subject = (Subject) o;
-        return Objects.equals(name, subject.name);
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Subject) obj;
+        return Objects.equals(this.name, that.name) &&
+                this.score == that.score;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, score);
     }
 
     @Override
     public String toString() {
-        return "Subject{"
-                + "name='" + name + '\''
-                + ", score=" + score
-                + '}';
+        return "Subject[" +
+                "name=" + name + ", " +
+                "score=" + score + ']';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }

176cac617b675e585284a47a6b50b014c33c0966
samratov1988 <samratov1988@gmail.com>
2023-02-11 22:46:14 +0600

    4. Аттестация [#504878 #390439]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -3,7 +3,6 @@ package ru.job4j.hmap;
 import java.util.*;
 
 public class AnalyzeByMap {
-
     public static double averageScore(List<Pupil> pupils) {
         double sum = 0;
         int count = 0;
